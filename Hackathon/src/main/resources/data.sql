60be8c9a9fac82bb820d4532a4441de7a624419d
Hackathon2023 <Hackathon2023@DESKTOP-CEDN8NJ>
2023-05-25 14:09:41 +0200

    Removed (20)

--- Hackathon/src/main/resources/data.sql
+++ Hackathon/src/main/resources/data.sql
@@ -18,7 +18,7 @@ CREATE TABLE roles(
 );
 
 CREATE TABLE user_roles(
-	user_id INT(20) NOT NULL,
+	user_id INT NOT NULL,
     role_id INT NOT NULL,
     PRIMARY KEY (user_id, role_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE, 
