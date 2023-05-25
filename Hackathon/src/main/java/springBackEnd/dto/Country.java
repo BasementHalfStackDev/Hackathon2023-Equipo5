0ec275922e399f673066bdd2fcaa3a359e617024
AnnaSantasusana <annasberch@gmail.com>
2023-05-25 14:07:06 +0200

    many-to-one

--- Hackathon/src/main/java/springBackEnd/dto/Country.java
+++ Hackathon/src/main/java/springBackEnd/dto/Country.java
@@ -3,14 +3,13 @@
  */
 package springBackEnd.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -44,5 +43,7 @@ public class Country {
 	private double earths_required;
 	private double countries_required;
 	private String data_quality;
+	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<User> users = new ArrayList<>();
 
 }
--- Hackathon/src/main/java/springBackEnd/dto/User.java
+++ Hackathon/src/main/java/springBackEnd/dto/User.java
@@ -41,6 +41,10 @@ public class User {
 	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+
 	// Constructors
 	public User() {
 	}
